ad7f64ddadbb8ea6d73172ad26c1ebe741e2b3a3
Aek <ekkawin.v@gmail.com>
2022-07-20 23:03:25 +0700

    fizz buss

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,2 +1,15 @@
-console.log("hello world");
-console.log("I push wrong commit");
+let i = 1;
+let text = "";
+
+for (i = 1; i <= 100; i++) {
+  if (i % 3 == 0) {
+    text = text + "Fizz ";
+  } else if (i % 5 == 0) {
+    text = text + "Buzz ";
+  } else {
+    text = text + i + " ";
+  }
+  if (i % 6 == 0) {
+    text = text + "\n";
+  }
+}
